a90be61f22b43bc169dd7723282098be917d401f
Siddharth Thakur <Siddharth.Thakur@nissan-usa.com>
2016-01-17 18:30:04 +0530

    Removed local config
    Added git ignore
    
    Removed swp files

--- user_auth/app/app.py
+++ user_auth/app/app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 client = MongoClient( os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
-#client = MongoClient( "192.168.99.100", 27017)
 
 # Instantiate the database object
 ride_sharing = client.ride_sharing
@@ -56,5 +55,4 @@ def auth_user():
         pass
 
 if __name__ == '__main__':
-    #app.run(host='127.0.0.1', debug=True)
     app.run(host='0.0.0.0', debug=True)
